1390a29afa6d3078be263fd86919b4b8dbf96e05
Pieter du Plessis <pduplessis@jhb.dvt.co.za>
2022-09-21 18:24:16 +0200

    Add db connection to postgres db and also cleanup docker file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/PieterDup98/go-rest-api-course/db"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to the database")
+		return err
+	}
+	//Not needed as it's already done above
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged the database")
 	return nil
 }
 
